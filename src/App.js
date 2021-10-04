58dde6e3299bddf5ecb93cba060a2e0c1461ae5f
BeatrixDev <beatrix.ford@hotmail.com>
2021-10-04 17:17:19 -0300

    feat: add code App file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import ProductList from "./components/productList/productList";
+import { CatalogueProvider } from "./providers/catalogue";
+import { CartProvider } from "./providers/cart";
+import { Container } from "./style";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <CatalogueProvider>
+        <CartProvider>
+          <ProductList type="catalogue" />
+          <ProductList type="cart" />
+        </CartProvider>
+      </CatalogueProvider>
+    </Container>
   );
 }
 
